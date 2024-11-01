67f8c62002476d6f17510bb364c119b7bdb38c62
Leonardo <st10151@iismarconipieralisi.it>
2024-11-01 15:32:29 +0100

    deleted all the comments and better style

--- src/p12/exercise/MultiQueueImpl.java
+++ src/p12/exercise/MultiQueueImpl.java
@@ -1,7 +1,6 @@
 package p12.exercise;
 
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -12,64 +11,38 @@ import java.util.TreeSet;
 
 public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
 
-    Map<Q,Queue<T>> map;
-    //Queue<T>[] code = new Queue[50]; 
-    //int ne=0;
+    private Map<Q,Queue<T>> map;
+
 
     public MultiQueueImpl(){
 
-        /*
-        for(int i=0;i<50;i++){
-            code[i] = new LinkedList<T>();
-        }*/
-        map = new TreeMap<>();
+        map = new HashMap<>();
+
     }
 
 
 
     @Override
     public Set<Q> availableQueues() {
-        /*Set<Q> set = new HashSet<>();
-
-        for(int i=0;i<ne;i++){
-
-            set.add(map.get(i));
-        } 
-
-        return set;*/
         
         
         Set<Q> set = new TreeSet<>();
-        for(Q key : map.keySet()){
-            set.add(key);
-        }
-        
+        set.addAll(map.keySet());
         return set;
     }
 
     @Override
     public void openNewQueue(Q queue) {
-        //map.put(ne, queue);
-        //ne++;
-
-        if(existQueue(queue)) throw new IllegalArgumentException();
+    
+        if(map.containsKey(queue)) throw new IllegalArgumentException();
         map.put(queue, new LinkedList<T>());
 
-    }
-
-    private boolean existQueue(Q queue){
-        for(Q q : map.keySet()){
-            if(q.equals(queue)){
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
     public boolean isQueueEmpty(Q queue) {
         
-        if(!existQueue(queue)) throw new IllegalArgumentException();
+        if(!map.containsKey(queue)) throw new IllegalArgumentException();
         return map.get(queue).isEmpty();
 
 
@@ -77,31 +50,15 @@ public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
 
     @Override
     public void enqueue(T elem, Q queue) {
-       /*int index=-1;
-        boolean trovato=false;
-        for(int i=0;i<ne;i++){
-
-            if(queue.equals(map.get(i))){
-                index=i;
-                trovato=true;
-            }
-        }
-        if(trovato){
-
-            System.out.println(code[index]);
-            code[index].add(elem);
-        }else{
-            System.out.println("Cassa non trovata");
-
-        }*/
-        if(!existQueue(queue)) throw new IllegalArgumentException();
+       
+        if(!map.containsKey(queue)) throw new IllegalArgumentException();
         map.get(queue).add(elem);
         
     }
 
     @Override
     public T dequeue(Q queue) {
-        if(!existQueue(queue)) throw new IllegalArgumentException();
+        if(!map.containsKey(queue)) throw new IllegalArgumentException();
         return map.get(queue).poll();
     }
 
@@ -115,6 +72,7 @@ public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
 
         }
         return test;
+
     }
 
     @Override
@@ -136,11 +94,9 @@ public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
     @Override
     public List<T> dequeueAllFromQueue(Q queue) {
         
-        if(!existQueue(queue)) throw new IllegalArgumentException();
+        if(!map.containsKey(queue)) throw new IllegalArgumentException();
         List<T> prova = new LinkedList<>();
-
         while(!(map.get(queue).isEmpty())){
-
             prova.add(map.get(queue).poll());
         }
 
@@ -150,21 +106,12 @@ public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
     @Override
     public void closeQueueAndReallocate(Q queue) {
 
-        if(!existQueue(queue)) throw new IllegalArgumentException();
+        if(!map.containsKey(queue)) throw new IllegalArgumentException();
         if(map.keySet().size()==1) throw new IllegalStateException();
         List<T> prova = dequeueAllFromQueue(queue);
         map.remove(queue);
         map.get(map.keySet().iterator().next()).addAll(prova);
         
-        /*
-        for(Q q : map.keySet()){
-            if(!q.equals(queue)){
-                map.get(q).addAll(dequeueAllFromQueue(queue));
-                break;
-            }
-        }
-
-        map.remove(queue);*/
         
     }   
 
